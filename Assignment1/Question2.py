0d73f6233b48d53d21384d94bc2544d15e345d93
grant81 <grantzhao75@gmail.com>
2018-01-25 12:39:58 -0500

    almost finish Q 2

--- Assignment1/Question2.py
+++ Assignment1/Question2.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -17,18 +18,18 @@ def getWByGD(trainPath,stepSize,numStep):#'Datasets\\Dataset_2_train.csv'
         for i in range(len(xTemp)):
             one.append(1.0)
 
-        x.append(one)
+
         x.append(xTemp)
+        x.append(one)
         y = np.transpose(np.array(y, dtype='float'))
         x = np.transpose(np.array(x,dtype='float'))
 
-    step1 = np.dot(np.transpose(x),x)
-    step2 = np.dot(np.transpose(x),y)
-    w = np.dot(np.linalg.inv(step1),step2)
+    w= np.array([1.0,1.0],dtype='float')
     MSEList = []
-    with open('Datasets\\Dataset_2_train.csv') as validData:
+    with open('Datasets\\Dataset_2_valid.csv') as validData:
         readCSV = csv.reader(validData, delimiter=',')
         xTemp = []
+        one =[]
         x1 = []
         y1 = []
 
@@ -36,29 +37,31 @@ def getWByGD(trainPath,stepSize,numStep):#'Datasets\\Dataset_2_train.csv'
             xTemp.append(float(row[0]))
             y1.append(float(row[1]))
 
+        for i in range(len(xTemp)):
+            one.append(1.0)
 
+        x1.append(xTemp)
         x1.append(one)
-        x1.append(xTemp) #(1,x)
         y1 = np.transpose(np.array(y1, dtype='float'))
         x1 = np.transpose(np.array(x1,dtype='float'))
 
     for i in range (numStep):
         for j in range (x.shape[0]):
-            w[0] = w[0] - stepSize*(w[0]+w[1]*x[j][1] - y[j])
-            w[1] = w[1] - stepSize * (w[0] + w[1] * x[j][1] - y[j])*x[j][1]
+            w[1] = w[1] - stepSize*(w[1]+w[0]*x[j][0] - y[j])
+            w[0] = w[0] - stepSize * (w[1] + w[0] * x[j][0] - y[j])*x[j][0]
         temp = []
         temp.append(i)
         temp.append(calMSE(x1,y1,w))
         MSEList.append(temp)
     print(MSEList)
     plt.plot(MSEList,'ro')
-    plt.axis([0,20000,0.09,0.2])
+    plt.axis([0,20000,0,30])
     plt.show()
 
     return w
 
 def calTarget (x , w):
-    y = w[0] + x * w[1]
+    y = w[1] + x * w[0]
 
     return y
 
@@ -75,7 +78,7 @@ def produceResultPoint (testDataPath,w):
         for i in range(len(inputVar)):
             result.append(calTarget(inputVar[i],w))
 
-    plt.plot(inputVar,result,'g^')
+    plt.plot(inputVar,result,'g-')
     plt.plot(inputVar,targetVar,'ro')
     plt.show()
 
@@ -96,10 +99,12 @@ def calMSE (x,y,w):
     #     x.append(one)
     #     y = np.transpose(np.array(y, dtype='float'))
     #     x = np.transpose(np.array(x,dtype='float'))
+    temp = w[0]
+    w[0] = w[1]
+    w[1] =temp
     YsXw = np.subtract(np.dot(x,w),y)
-
     Error = np.square(YsXw).mean()
     return Error
-w = getWByGD('Datasets\\Dataset_2_train.csv',stepSize,20000)
+w = getWByGD('Datasets\\Dataset_2_train.csv',stepSize,10000)
 # print("MSE = "+str(calMSE('Datasets\\Dataset_2_valid.csv',w)))
 produceResultPoint('Datasets\\Dataset_2_valid.csv',w)
