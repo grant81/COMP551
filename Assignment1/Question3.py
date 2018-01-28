31a29776daf22dca8285f59fd2e890eded18d68d
grant81 <grantzhao75@gmail.com>
2018-01-28 16:42:15 -0500

    pre submission

--- Assignment1/Question3.py
+++ Assignment1/Question3.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 import csv
 from random import shuffle
 
@@ -15,7 +15,7 @@ def prepareData(dataPath, statPath):
         stat = []
         for row in readCSV:
             stat.append(row)
-        print(len(data) / 5, len(data[0]))
+
 
     for j in range(len(data)):
         for i in range(5, len(data[0])):
@@ -84,6 +84,7 @@ def getWRidge(trainSetPath, lamda):
             targetVarTemp.append(float(row[len(row) - 1]))
             targetVar.append(targetVarTemp)
 
+
     inputVar = np.array(inputVar, dtype='float')
     targetVar = (np.array(targetVar, dtype='float'))
     step1 = np.dot(np.transpose(inputVar), inputVar)
@@ -112,10 +113,9 @@ def produceResultPoint(testDataPath, w):
     inputVar = np.array(inputVar, dtype='float')
     targetVar = (np.array(targetVar, dtype='float'))
     result = np.dot(inputVar, w)
-    # for i in range(len(targetVar)):
-    #     print(targetVar[i],result[i])
-    plt.plot(result, 'b^')
-    plt.plot(targetVar, 'ro')
+    plt.plot(result, 'b^',label = 'result')
+    plt.plot(targetVar, 'ro', label = 'target')
+    plt.legend()
     plt.show()
 
 
@@ -143,20 +143,24 @@ def calMSE(testDataPath, w):
 
 
 def findLamda():
-    lamda = 0.0
+    lamda = 1e-9
     MSEAverage = []
     lamdas = []
-    while (lamda <= 5):
+    Ws = []
+    while (lamda <= 10000):
         MSEs = []
+        Wtemp = []
         for i in range(1, 6):
             w = getWRidge('Datasets\\CandC-train' + str(i) + '.csv', lamda)
             mse = calMSE('Datasets\\CandC-test' + str(i) + '.csv', w)
             MSEs.append(mse)
+            Wtemp.append(w)
         meanMSE = np.array(MSEs, dtype='float').mean()
-        print(meanMSE)
+#         print(meanMSE)
         MSEAverage.append(meanMSE)
-        lamdas.append(lamda)
-        lamda += 0.025
+        lamdas.append(math.log10(lamda))
+        Ws.append(Wtemp)
+        lamda = lamda * 10
     minErr = 0.0
     minLamda = 0.0
     for i in range(len(lamdas)):
@@ -165,30 +169,138 @@ def findLamda():
             minLamda = lamdas[i]
         elif (minErr > MSEAverage[i]):
             minErr = MSEAverage[i]
-            minLamda = lamdas[i]
+            minLamda = 10**lamdas[i]
+            minW = Ws[i]
+    print('the optimal lamda is = '+str(minLamda)+', produce an Average MSE of '
+             +str(minErr))
+    # plt.axis([0, 13, 0, 8])
 
-    print(minErr, minLamda)
-    plt.axis([0, 5, 0, 10])
     plt.plot(lamdas, MSEAverage)
+    plt.title('MSE vs Lamda')
+    plt.xlabel('log(Lamda)')
+    plt.ylabel('MSE')
     plt.show()
 
 
 # MSEs = []
-# Ws = []
+
 #
 # for i in range(1, 6):
 #     w = getWRidge('Datasets\\CandC-train' + str(i) + '.csv', 5)
 #     # Ws.append(w)
 #     produceResultPoint('Datasets\\CandC-test' + str(i) + '.csv', w)
-#     mse = calMSE('Datasets\\CandC-test' + str(i) + '.csv', w)
-#     print(mse)
-#     MSEs.append(mse)
-#     # findLamda()
+#
+# # findLamda()
+
+
+def getWRidgeFS(trainSetPath,lamda,removedFeatures):
+    with open(trainSetPath) as trainData1:
+        readCSV = csv.reader(trainData1, delimiter=',')
+        inputVar = []
+        targetVar = []
+
+        for row in readCSV:
+            inputVarTemp = []
+            for i in range(5, len(row) - 1):
+                if ((i == removedFeatures).any()):
+                    pass
+                else:
+                    inputVarTemp.append(float(row[i]))
+            inputVarTemp.append(1.0)
+            inputVar.append(inputVarTemp)
+            targetVarTemp = []
+            targetVarTemp.append(float(row[len(row) - 1]))
+            targetVar.append(targetVarTemp)
+
+
+    inputVar = np.array(inputVar, dtype='float')
+    targetVar = (np.array(targetVar, dtype='float'))
+    step1 = np.dot(np.transpose(inputVar), inputVar)
+    step2 = np.add(step1, lamda * np.identity(step1.shape[0]))
+    step3 = np.linalg.inv(step2)
+    step4 = np.dot(np.transpose(inputVar), targetVar)
+    w = np.dot(step3, step4)
+    return w
+
+def produceResultPointFS(testDataPath, w,removedFeatures):
+    with open(testDataPath) as trainData1:
+        readCSV = csv.reader(trainData1, delimiter=',')
+        inputVar = []
+        targetVar = []
+
+        for row in readCSV:
+            inputVarTemp = []
+            for i in range(5, len(row) - 1):
+                if ((i == removedFeatures).any()):
+                    pass
+                else:
+                    inputVarTemp.append(float(row[i]))
+            inputVarTemp.append(1.0)
+            inputVar.append(inputVarTemp)
+            targetVarTemp = []
+            targetVarTemp.append(float(row[len(row) - 1]))
+            targetVar.append(targetVarTemp)
+    inputVar = np.array(inputVar, dtype='float')
+    targetVar = (np.array(targetVar, dtype='float'))
+    result = np.dot(inputVar, w)
+    plt.plot(result, 'b^',label = 'result')
+    plt.plot(targetVar, 'ro', label = 'target')
+    plt.legend()
+    plt.show()
+
+
+def calMSEFS(testDataPath, w ,removedFeatures):
+    with open(testDataPath) as trainData1:
+        readCSV = csv.reader(trainData1, delimiter=',')
+        inputVar = []
+        targetVar = []
+
+        for row in readCSV:
+            inputVarTemp = []
+            for i in range(5, len(row) - 1):
+                if ((i == removedFeatures).any()):
+                    pass
+                else:
+                    inputVarTemp.append(float(row[i]))
+            inputVarTemp.append(1.0)
+            inputVar.append(inputVarTemp)
+            targetVarTemp = []
+            targetVarTemp.append(float(row[len(row) - 1]))
+            targetVar.append(targetVarTemp)
+    inputVar = np.array(inputVar, dtype='float')
+    targetVar = (np.array(targetVar, dtype='float'))
+
+    YsXw = np.subtract(targetVar, np.dot(inputVar, w))
+    Error = np.square(YsXw).mean()
+    return Error
+
 Ws = []
+for i in range(1, 6):
+    w = getWRidge('Datasets\\CandC-train' + str(i) + '.csv', 1.0)
+    Ws.append(np.array(w))
+
+WAvg = []
+for j in range(0, 123):
+    rowTemp = []
+    sum = 0.0
+    rowTemp.append(Ws[0][j][0])
+    rowTemp.append(Ws[1][j][0])
+    rowTemp.append(Ws[2][j][0])
+    rowTemp.append(Ws[3][j][0])
+    rowTemp.append(Ws[4][j][0])
+
+    for k in range(len(rowTemp)):
+        sum += abs(rowTemp[k])
+    WAvg.append(sum/len(rowTemp))
+WAvg = np.array(WAvg,dtype='float')
+smallValues = WAvg.argsort()[0:22]
+
+# smallValues = np.array(smallValues.tolist())
+# print(smallValues)
+
 
 for i in range(1, 6):
-    w = getWRidge('Datasets\\CandC-train' + str(i) + '.csv', 5)
-    Ws.append(np.array(w))
-for j in range(0,123):
-    print(Ws[0][j][0],Ws[1][j][0],Ws[2][j][0],Ws[3][j][0],Ws[4][j][0])
-    print(np.array(Ws).mean())
+    w = getWRidgeFS('Datasets\\CandC-train' + str(i) + '.csv', 1.0,smallValues)
+    mse = calMSEFS('Datasets\\CandC-test' + str(i) + '.csv', w,smallValues)
+    print('the MSE for 80-20 split ' + str(i) + ' with feature selection is ' + str(mse))
+    produceResultPointFS('Datasets\\CandC-test' + str(i) + '.csv', w,smallValues)
